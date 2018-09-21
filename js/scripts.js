c60761b6d4851a51e5890dc00099a79fe5c3bf55
Ryan Wu <ryanausdeutschland@gmail.com>
2018-09-21 19:59:39 +0200

    new ink tank page, minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 console.log("Hello world")
+$(document).ready(function(){
+    $("#work").click(function(){
+        $("#containerwork").slideToggle("slow");
+    });
+});
+</script>
+<script>
+$(document).ready(function(){
+    $("#about-me").click(function(){
+        $("#aboutmecontent").slideToggle("slow");
+    });
+});
